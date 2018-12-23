561c21c20a3b91a1fa907e585485ca2fd9c7c941
Chmouel Boudjnah <chmouel@chmouel.com>
2018-12-24 00:47:15 +0100

    fixes

--- showplan.py
+++ showplan.py
@@ -112,7 +112,7 @@ def show_plan(args):
         for day in list(calendar.day_name):
             cursor_date = cursor_date + datetime.timedelta(days=1)
 
-            tdd = datetime.datetime.now().strftime("%Y%m%d")
+            tdd = datetime.datetime.now()
             if args.today and \
                (cursor_date.strftime("%Y%m%d") != tdd.strftime("%Y%m%d")):
                 continue
